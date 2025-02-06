bbd601fd67c85c6b3613c4225aaba9ae694edffb
Dominic Sisneros <dsisnero@gmail.com>
2025-02-06 13:33:56 -0700

    refactor: Remove FileStorage class and simplify project directory creation

--- src/faa/project_dir.cr
+++ src/faa/project_dir.cr
@@ -1,33 +1,6 @@
 require "./project_dir"
 require "baked_file_system"
-
-class FileStorage
-  Logger = Log.for(self)
-  extend BakedFileSystem
-
-  @@baked_files : Array(BakedFileSystem::BakedFile) = begin
-    [] of BakedFileSystem::BakedFile
-  end
-    
-
-  class_getter(baked_files) { BakedFileSystemMounter::BakedFileSystemStorage.baked_files }
-
-  bake_folder "../../project_lib"
-
-  def self.copy_to(dir : String | Path)
-    base = Path.new(dir)
-    raise "Not a dir" unless File.directory? base
-    baked_files.each do |file|
-        path = base / file.path
-        dir = path.parent
-        unless File.directory? dir
-          Logger.debug {"making dir #{dir}"}
-          FileUtils.mkdir_p(dir)
-        end
-        File.write(full_path, file.gets_to_end)
-    end
-  end
-end
+require "baked_file_system_mounter"
 
 
 
@@ -47,9 +20,10 @@ module Faa
       ::Dir.empty? dir
     end
 
+    # make project directories and files using the project_lib files
+    # make them a subdirectory of dir
     def make_subdirectories
       return unless empty?
-      FileStorage.copy_to(dir)
     end
 
   end
