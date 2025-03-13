64af9759b209d6284ded9d1d2335c4df906b13a5
Dominic Sisneros <dsisnero@gmail.com>
2025-03-12 19:26:05 -0600

    refactor: Simplify test helper methods with block syntax and cleanup

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -19,46 +19,45 @@ ensure
 end
 
 # Mock the Faa::Config.load to use test directory
-def with_test_config
+def with_test_config(&)
   original_config = Faa::Config.load
   test_config = Faa::Config.new
   test_config.working_project_directory = Dir.tempdir
   test_config.active_project_library = Dir.tempdir
-  
+
   yield test_config
 ensure
   original_config.try(&.save)
 end
 
-def capture_stderr
+def capture_stderr(&)
   # Create a pipe for capturing output
   reader, writer = IO.pipe
   original_stderr = STDERR.dup
-  
+
   # Redirect stderr to our pipe writer
   STDERR.reopen(writer)
   yield
-  
+
   # Close writer and read output
   writer.close
   reader.gets_to_end
 ensure
   # Restore original stderr
-  STDERR.reopen(original_stderr)
+  STDERR.reopen(original_stderr.not_nil!)
   reader.try(&.close)
   writer.try(&.close)
 end
 
-def capture_exit_code
+def capture_exit_code(&)
   exit_code = 0
   begin
     yield
-  rescue ex : Crystal::SystemExit
-    exit_code = ex.status
+  rescue ex : ::Exception
+    exit_code = ex.message
   end
   exit_code
 end
-
 
 require "./faa/dir_spec"
 require "./faa/utils_spec"
